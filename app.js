78b6149752764643982b3ea42eda830d642430ee
Lhoussaine <bouaik.lhou@gmail.com>
2020-09-05 13:05:54 +0100

    make logic for buttons to counte

---
+++ app.js
@@ -0,0 +1,31 @@
+(function () {
+    let counterValue = 0
+    const buttons = document.querySelectorAll('.counterBtn')
+    const counter = document.getElementById('counter')
+
+    buttons.forEach( (btn) => {
+        btn.addEventListener('click', (e) => {
+            const value = e.target
+
+            if (value.classList.contains('prevBtn')) {  
+                counterValue--
+            } else if (value.classList.contains('nextBtn')) {
+                counterValue++
+            }
+
+            counter.textContent = counterValue
+            
+            if (counterValue === 0) {
+                counter.style.color = "blue"
+            } else if (counterValue < 0) {
+                counter.style.color = "red"
+            } else {
+                counter.style.color = "green"
+            }
+        })
+    })
+
+
+
+
+})();
